e9c974b955055d6cbd5b14e2a8095762e1b6c031
Bourgond Aries <macocio@gmail.com>
2016-06-20 23:38:48 +0200

    Implement general collision handling!

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,7 @@ use sfml::audio::{Sound, SoundBuffer, SoundSource};
 use rand::{Rng, thread_rng};
 use std::f32::consts::PI;
 use std::env;
+use tile_net::*;
 
 fn main() {
 	let mut window = match RenderWindow::new(VideoMode::new_init(800, 600, 42),
@@ -20,7 +21,17 @@ fn main() {
 		None => panic!("SHIT"),
 	};
 
+	let mut block = RectangleShape::new().unwrap();
+	block.set_size(&Vector2f::new(100.0, 100.0));
+	block.set_fill_color(&Color::new_rgb(0, 0, 0));
+
+	let net = tile_net::TileNet::sample();
+
 	window.set_framerate_limit(60);
+
+	let per = 60;
+	let mut cur = 0;
+
 	'main: loop {
 		for event in window.events() {
 			match event {
@@ -34,7 +45,31 @@ fn main() {
 				_ => {},
 			}
 		}
+
+		let oldpos = block.get_position();
+		if Key::Up.is_pressed() {
+			block.move2f(0.0, -1.0);
+		}
+		if Key::Down.is_pressed() {
+			block.move2f(0.0, 1.0);
+		}
+		if Key::Left.is_pressed() {
+			block.move2f(-1.0, 0.0);
+		}
+		if Key::Right.is_pressed() {
+			block.move2f(1.0, 0.0);
+		}
+
+		let rect = block.get_global_bounds();
+		let left_bottom = Point(rect.left/100.0, (rect.top + rect.height)/100.0);
+		let right_bottom = Point((rect.left + rect.width)/100.0, (rect.top + rect.height)/100.0);
+		let line = Line(left_bottom, right_bottom);
+		if !net.collide_set(line.supercover()).all(|x| x == &None) {
+			block.set_position(&oldpos);
+		}
+
 		window.clear(&Color::new_rgb(200, 2, 3));
+		window.draw(&block);
 		window.display();
 	}
 }
