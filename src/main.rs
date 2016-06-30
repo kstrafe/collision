ec4c138dbb20c9743ac83c457be2b17bc06324de
Bourgond Aries <macocio@gmail.com>
2016-07-01 00:54:03 +0200

    Do a major code cleanup

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ use std::time::Duration;
 use std::sync::mpsc::{channel, Sender};
 
 use sfml::graphics::{CircleShape, Color, Font, RectangleShape, RenderTarget, RenderWindow, Shape,
-                     Text, Transformable};
+                     Text, Transformable, Drawable, RenderStates};
 use sfml::window::{ContextSettings, Key, VideoMode, event, window_style};
 use sfml::system::{Clock, Time, Vector2f};
 use sfml::audio::{Sound, SoundBuffer, SoundSource};
@@ -16,105 +16,128 @@ use std::f32::consts::PI;
 use std::env;
 use tile_net::*;
 
+#[derive(Debug)]
+struct Rects {
+	pts: Vec<(f32, f32)>,
+	pos: Vector,
+	mov: Vector,
+}
+
+impl Rects {
+	fn new() -> Rects {
+		Rects {
+			pts: vec![(0.0, 0.0), (1.0, 0.0), (0.0, 1.0), (1.0, 1.0)],
+			pos: Vector(1.0, 0.0),
+			mov: Vector(0.0, 0.0),
+		}
+	}
+
+	fn get_pos(&self) -> Vector {
+		self.pos
+	}
+}
+
+impl Collable for Rects {
+	fn points<'a>(&'a self) -> Points<'a> {
+		Points::new(self.pos, &self.pts)
+	}
+
+	fn enqueue(&mut self, vector: Vector) {
+		self.mov = self.mov + vector;
+	}
+
+	fn queued(&self) -> Vector {
+		self.mov
+	}
+
+	fn resolve<'a, T, I>(&mut self, mut set: TileSet<'a, T, I>) -> bool
+		where T: 'a,
+		      I: Iterator<Item = (i32, i32)>
+	{
+		let mov = self.mov;
+		self.mov = Vector(0.0, 0.0);
+		if set.all(Option::is_none) {
+			self.pos = self.pos + mov;
+			true
+		} else {
+			false
+		}
+	}
+}
+
+impl Drawable for Rects {
+	fn draw<R: RenderTarget>(&self, rt: &mut R, _: &mut RenderStates) {
+		let mut block = create_block();
+		block.set_position(&Vector2f::new(self.pos.0*100.0, self.pos.1*100.0));
+		rt.draw(&block);
+	}
+}
+
+struct Rare {
+	count: usize,
+	every: usize,
+}
+
+impl Rare {
+	fn new(every: usize) -> Rare {
+		Rare { count: 0, every: every }
+	}
+
+	fn run<F: Fn()>(&mut self, function: F) {
+		if self.count == self.every {
+			self.count = 0;
+			function();
+		} else {
+			self.count += 1;
+		}
+	}
+}
+
 fn main() {
 
-	let mut window = match RenderWindow::new(VideoMode::new_init(800, 600, 42),
-	                                         "Custom shape",
-	                                         window_style::CLOSE,
-	                                         &Default::default()) {
-		Some(window) => window,
-		None => panic!("SHIT"),
-	};
-
-	let mut block = RectangleShape::new().unwrap();
-	block.set_size(&Vector2f::new(100.0, 100.0));
-	block.set_fill_color(&Color::new_rgb(0, 0, 0));
-	block.set_position2f(100.0, 0.0);
-	let mut tile = RectangleShape::new().unwrap();
-	tile.set_size(&Vector2f::new(100.0, 100.0));
-	tile.set_fill_color(&Color::new_rgb(0, 200, 0));
-
-	let mut net = tile_net::TileNet::sample();
-	*net.get_mut((3, 2)).unwrap() = Some(0);
-	(0..6)
-		.map(|x| {
-			*net.get_mut((0, x)).unwrap() = Some(0);
-		})
-		.count();
-	*net.get_mut((3, 2)).unwrap() = Some(0);
-
-	window.set_framerate_limit(60);
-
-	let per = 60;
-	let mut cur = 0;
-	let mut speed = 10000000000.0;
+	let mut window = create_window();
+	let mut net = create_tilenet();
+	let mut tile = create_tile();
+	let mut coller = Rects::new();
+	let mut rarer = Rare::new(60);
 	let mut gravity = 0.0981;
 
 	'main: loop {
-		for event in window.events() {
-			match event {
-				event::Closed => break 'main,
-				event::KeyPressed { code, .. } => {
-					match code {
-						Key::Escape => break 'main,
-						_ => {}
-					}
-				}
-				_ => {}
-			}
+		if handle_events(&mut window) {
+			break 'main;
 		}
 
-		println!("{:?}", block.get_position());
 
 		let side_speed = 2.0;
-		let oldpos = block.get_position();
 		if Key::Up.is_pressed() {
-			speed -= 0.5;
-			block.move2f(0.0, -1.0);
+			coller.enqueue(Vector(0.0, -0.5));
 		}
 		if Key::Down.is_pressed() {
-			block.move2f(0.0, 1.0);
+			coller.enqueue(Vector(0.0, 0.5));
 		}
 		if Key::Left.is_pressed() {
-			block.move2f(-side_speed, 0.0);
+			coller.enqueue(Vector(-side_speed/100.0, 0.0));
 		}
 		if Key::Right.is_pressed() {
-			block.move2f(side_speed, 0.0);
+			coller.enqueue(Vector(side_speed/100.0, 0.0));
 		}
-		gravity = 0.0;
+		rarer.run(|| println!("{:?}", coller));
 
-		let rect = block.get_global_bounds();
-		let left_bottom = Vector(rect.left / 100.0, (rect.top + rect.height) / 100.0);
-		// block.move2f(0.0, speed);
-		let rect = block.get_global_bounds();
-		let right_bottom = Vector(rect.left / 100.0, (rect.top + rect.height) / 100.0);
-		let line = Line(left_bottom, right_bottom);
-		if !net.collide_set(line.supercover()).all(|x| x == &None) {
-			println!("Collided");
-			gravity = 0.0;
-			block.set_position(&oldpos);
-			speed = 0.0;
-		}
+		rarer.run(|| {
+			net.collide_set(coller.tiles()).inspect(|x|
+				println!("{:?}", x)
+			).count();
+			}
+		);
 
-		gravity = 0.0981;
-		speed += gravity;
-		let oldpos = block.get_position();
-
-		let rect = block.get_global_bounds();
-		let left_bottom = Vector(rect.left / 100.0, (rect.top + rect.height) / 100.0);
-		block.move2f(0.0, speed);
-		let rect = block.get_global_bounds();
-		let right_bottom = Vector(rect.left / 100.0, (rect.top + rect.height) / 100.0);
-		let line = Line(left_bottom, right_bottom);
-		if !net.collide_set(line.supercover()).all(|x| x == &None) {
-			println!("Collided");
-			gravity = 0.0;
-			block.set_position(&oldpos);
-			speed = 0.0;
+		let tiles = net.collide_set(coller.tiles());
+		if coller.resolve(tiles) {
+			coller.enqueue(Vector(0.0, 0.0981));
+			let tiles = net.collide_set(coller.tiles());
+			coller.resolve(tiles);
 		}
 
 		window.clear(&Color::new_rgb(200, 2, 3));
-		window.draw(&block);
 		for (index, i) in net.view_box((0, 10, 0, 10)).enumerate() {
 			if let &Some(_) = i {
 				let col = (index % 10) as f32;
@@ -123,6 +146,64 @@ fn main() {
 				window.draw(&tile);
 			}
 		}
+
+		window.draw(&coller);
+
 		window.display();
 	}
+}
+
+fn create_window() -> RenderWindow {
+	let mut window = RenderWindow::new(VideoMode::new_init(800, 600, 42),
+	                                   "Custom shape",
+	                                   window_style::CLOSE,
+	                                   &Default::default())
+		.unwrap_or_else(|| {
+			panic!("Could not create window");
+		});
+	window.set_framerate_limit(60);
+	window
+}
+
+fn create_tilenet() -> tile_net::TileNet<usize> {
+	let mut net = tile_net::TileNet::sample();
+	*net.get_mut((3, 2)).unwrap() = Some(0);
+	(0..6)
+		.map(|x| {
+			*net.get_mut((0, x)).unwrap() = Some(0);
+		})
+		.count();
+	*net.get_mut((3, 2)).unwrap() = Some(0);
+	net
+}
+
+fn create_block<'a>() -> RectangleShape<'a> {
+	let mut block = RectangleShape::new().unwrap();
+	block.set_size(&Vector2f::new(100.0, 100.0));
+	block.set_fill_color(&Color::new_rgb(0, 0, 0));
+	block.set_position2f(100.0, 0.0);
+	block
+}
+
+fn create_tile<'a>() -> RectangleShape<'a> {
+	let mut tile = RectangleShape::new().unwrap();
+	tile.set_size(&Vector2f::new(100.0, 100.0));
+	tile.set_fill_color(&Color::new_rgb(0, 200, 0));
+	tile
+}
+
+fn handle_events(window: &mut RenderWindow) -> bool {
+	for event in window.events() {
+		match event {
+			event::Closed => return true,
+			event::KeyPressed { code, .. } => {
+				match code {
+					Key::Escape => return true,
+					_ => {}
+				}
+			}
+			_ => {}
+		}
+	}
+	false
 }
